3330def27df7f94d38b76c51db682648e827bbe8
NissimBet <nissimbet123@outlook.com>
2020-04-12 14:15:30 -0500

    nose

--- lexer.py
+++ lexer.py
@@ -1,9 +1,9 @@
 # ------------------------------------------------------------
 # lexer.py
 # Alejandro Longoria y Nissim Betesh
-# 
+#
 # lex example for simple Covid19 Language
-# using PLY (a lex/yacc implementation for Python) 
+# using PLY (a lex/yacc implementation for Python)
 # code samples from: https://ply.readthedocs.io/en/latest/ply.html
 # ------------------------------------------------------------
 
@@ -11,86 +11,73 @@ import ply.lex as lex
 
 # Rule to match reserved words and identifiers
 reserved = {
-    'program':'PROGRAM',
-    'print':'PRINT',
-    'if':'IF',
-    'else':'ELSE',
-    'var':'VAR',
-    'int':'INT',
-    'float':'FLOAT'
+    'program': 'PROGRAM',
+    'print': 'PRINT',
+    'if': 'IF',
+    'else': 'ELSE',
+    'var': 'VAR',
+    'int': 'INT',
+    'float': 'FLOAT'
 }
 
 # List of token names.   This is always required
 tokens = [
-    'MULT',
-    'DIV',
-    'MAS',
-    'MENOS',
-    'IGUAL',
-    'MAYOR',
-    'MENOR',
+
     'DESIGUAL',
-    'PARIZQ',
-    'PARDER',
-    'BRACKETIZQ',
-    'BRACKETDER',
-    'PUNTOCOMA',
-    'COMA',
-    'DOSPUNTOS',
     'CTE_F',
     'CTE_I',
     'ID',
     'CTE_STRING'
-    ] + list(reserved.values())
+] + list(reserved.values())
+
+literals = '+-*/=<>(){}:;,'
 
 # Regular expression rules for simple tokens
-t_MULT          = r'\*'
-t_DIV           = r'/'
-t_MAS           = r'\+'
-t_MENOS         = r'-'
-t_IGUAL         = r'='
-t_DESIGUAL      = r'<>'
-t_MAYOR         = r'>'
-t_MENOR         = r'<'
-t_PARIZQ        = r'\('
-t_PARDER        = r'\)'
-t_BRACKETIZQ    = r'{'
-t_BRACKETDER    = r'}'
-t_DOSPUNTOS     = r':'
-t_PUNTOCOMA     = r';'
-t_COMA          = r','
-t_CTE_STRING     = r'\".*\"'
+t_DESIGUAL = r'<>'
+t_CTE_STRING = r'\".*\"'
 
 # Expression rule to detect and convert floating point numbers
+
+
 def t_CTE_F(t):
     r'(\d+[.])\d+'
     t.value = float(t.value)
     return t
 
 # A regular expression rule with some action code
+
+
 def t_CTE_I(t):
     r'\d+'
     t.value = int(t.value)
     return t
 
 # Expression rule to match possible id names
+
+
 def t_ID(t):
     r'[a-zA-Z][a-zA-Z0-9]*'
-    t.type = reserved.get(t.value,'ID') # Check for reserved words
+    t.type = reserved.get(t.value, 'ID')  # Check for reserved words
     return t
 
 # Define a rule so we can track line numbers
+
+
 def t_newline(t):
     r'\n+'
     t.lexer.lineno += len(t.value)
 
+
 # A string containing ignored characters (spaces and tabs)
-t_ignore  = ' \t'
+t_ignore = ' \t'
 
 # Error handling rule
+
+
 def t_error(t):
     print("Illegal character '%s'" % t.value[0])
     t.lexer.skip(1)
+
 
 lex.lex()
 
@@ -98,20 +85,20 @@ lex.lex()
 
 # data = '''
 # program donpato;
-# var numero:float; 
+# var numero:float;
 # {
 #     numeroPi = 3.1;
 # 	if (numeroPi < 3.14) {
 # 		numeroPi = 3.14159;
-# 	}  
-#     else 
+# 	}
+#     else
 #     {
 # 		print("Coronavirus will destroy math");
 # 	};
 # }
 # '''
 
-# Give the lexer some input
+# # Give the lexer some input
 # lex.input(data)
 
 # # Tokenize
--- parser.py
+++ parser.py
@@ -1,140 +1,171 @@
 # ------------------------------------------------------------
 # parser.py
 # Alejandro Longoria y Nissim Betesh
-# 
+#
 # yacc example for simple Covid19 Language
-# using PLY (a lex/yacc implementation for Python) 
+# using PLY (a lex/yacc implementation for Python)
 # code samples from: https://ply.readthedocs.io/en/latest/ply.html
 # ------------------------------------------------------------
 
 import ply.yacc as yacc
 
 # Get the token map from the lexer.  This is required.
-from lexer import tokens
+from lexer import tokens, literals
+
 
 def p_programa(p):
-    '''programa : PROGRAM ID PUNTOCOMA programaP bloque'''
+    '''programa : PROGRAM ID ';' programaP bloque'''
     p[0] = 'COMPILA'
+
 
 def p_programaP(p):
     '''programaP :  vars 
                     | epsilon '''
 
+
 def p_vars(p):
     '''vars : VAR varsPP'''
 
+
 def p_varsP(p):
-    '''varsP : COMA ID varsP
+    '''varsP : ',' ID varsP
                 | epsilon '''
 
+
 def p_varsPP(p):
-    '''varsPP : ID varsP DOSPUNTOS tipo PUNTOCOMA varsPP
+    '''varsPP : ID varsP ':' tipo ';' varsPP
                 | epsilon '''
+
 
 def p_tipo(p):
     '''tipo : INT 
             | FLOAT'''
 
+
 def p_bloque(p):
-    '''bloque : BRACKETIZQ bloqueP BRACKETDER'''
+    '''bloque : '{' bloqueP '}' '''
+
 
 def p_bloqueP(p):
     '''bloqueP : estatuto bloqueP
                         | epsilon'''
 
+
 def p_estatuto(p):
     '''estatuto : asignacion
                 | condicion
                 | escritura '''
-                
+
+
 def p_asignacion(p):
-    '''asignacion : ID IGUAL expresion PUNTOCOMA'''
+    '''asignacion : ID '=' expresion ;'''
+
 
 def p_expresion(p):
     '''expresion : exp expresionP'''
+
 
 def p_expresionP(p):
     '''expresionP : expresionPP exp
                     | epsilon '''
 
+
 def p_expresionPP(p):
-    '''expresionPP : MAYOR
-                    | MENOR
+    '''expresionPP : '>'
+                    | '<'
                     | DESIGUAL'''
+
 
 def p_exp(p):
     '''exp : termino expP'''
+
 
 def p_expP(p):
     '''expP : expPP termino expP
             | epsilon '''
 
+
 def p_expPP(p):
-    '''expPP : MAS
-            | MENOS '''
+    '''expPP : '+'
+            | '-' '''
+
 
 def p_termino(p):
     '''termino : factor terminoP'''
+
 
 def p_terminoP(p):
     '''terminoP : terminoPP factor terminoP
                 | epsilon'''
 
+
 def p_terminoPP(p):
-    '''terminoPP : MULT
-                | DIV '''
+    '''terminoPP : '*'
+                | '-' '''
+
 
 def p_factor(p):
-    '''factor : PARIZQ expresion PARDER 
+    '''factor : '(' expresion ')'
                 | factorP var_cte'''
 
+
 def p_factorP(p):
-    '''factorP : MAS
-                | MENOS
+    '''factorP : '+'
+                | '-'
                 | epsilon '''
 
+
 def p_escritura(p):
-    '''escritura : PRINT PARIZQ escrituraP PARDER PUNTOCOMA'''
+    '''escritura : PRINT '(' escrituraP ')' ';' '''
+
 
 def p_escrituraP(p):
     '''escrituraP : expresion escrituraPP
                 | CTE_STRING escrituraPP'''
 
+
 def p_escrituraPP(p):
-    '''escrituraPP : COMA escrituraP
+    '''escrituraPP : ',' escrituraP
                     | epsilon'''
 
+
 def p_condicion(p):
-    '''condicion : IF PARIZQ expresion PARDER bloque condicionP PUNTOCOMA'''
+    '''condicion : IF '(' expresion ')' bloque condicionP ';' '''
+
 
 def p_condicionP(p):
     '''condicionP : ELSE bloque 
                         | epsilon'''
+
 
 def p_var_cte(p):
     '''var_cte : ID 
                 | CTE_I 
                 | CTE_F '''
 
+
 def p_epsilon(p):
     '''epsilon :'''
 
 # Error rule for syntax errors
+
+
 def p_error(p):
     print("Syntax error in input!")
+
 
 # Build the parser
 parser = yacc.yacc()
 
 # data = '''
 # program donpato;
-# var numero:float; 
+# var numero:float;
 # {
 #     numeroPi = 3.1;
 # 	if (numeroPi < 3.14) {
 # 		numeroPi = 3.14159;
-# 	}  
-#     else 
+# 	}
+#     else
 #     {
 # 		print("Coronavirus will destroy math");
 # 	};
@@ -142,4 +173,6 @@ parser = yacc.yacc()
 # '''
 
 # if (parser.parse(data, tracking=True) == 'COMPILA'):
-#         print ("Sintaxis aceptada")
+#     print("Sintaxis aceptada")
+# else:
+#     print("error de sintaxis")
