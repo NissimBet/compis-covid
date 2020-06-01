805a9b6b25c653faf85d64535a25a8b08a1afb31
NissimBet <nissimbet123@outlook.com>
2020-06-01 17:02:12 -0500

    documentacion de AVAIL.py

--- compiler/AVAIL.py
+++ compiler/AVAIL.py
@@ -3,6 +3,7 @@ from typing import Union, Dict, Any
 
 class AVAIL:
     counter = 0
+    # Estructura base de las variables
     types: Dict[str, Dict[str, Dict[str, Dict[str, Any]]]] = {
         "global": {
             "non_temp": {},
@@ -13,22 +14,31 @@ class AVAIL:
             "temp": {}
         },
         "constant": {
-            "value": {}
+            "value": {}     # Este nivel es solo para tener consistencia con los niveles jerarquicos
+                            # y que el typing no tenga problemas
         }
     }
+    # los mapas de las direcciones a las variables es para tener una forma rapida
+    # de accesar y revisar la existencia de las variables sin hacer comparaciones a costa del uso de memoria
+    # mapeo de direccion a variable
     dir_to_var: Dict[str, str]
+    # mapeo de direccion a valor constante
     dir_to_const: Dict[str, Dict[str, str]]
+    # mapeo de valor constante a direccion
     const_to_dir: Dict[str, str]
 
     def __init__(self) -> None:
-        # self.counter = 0
+        # las categorias en que las variables se dividen y la direccion base
         categs = [("global", "non_temp", 1000), ("global", "temp", 2000),
                   ("local", "non_temp", 5000), ("local", "temp", 6000),
                   ("constant", "value", 10000)]
+        # los tipos de datos existentes y sus tamanos
         self.data_types = [("int", 100), ("float", 100),
                            ("char", 100), ("string", 100),
                            ("bool", 100), ("dataFrame", 100),
                            ("pointer", 100)]
+        # llenar los datos iniciales de la estructura del AVAIL
+        # iterar por las categorias y los tipos y inicializar direcciones base, contador y tamano de la variable actual
         for category in categs:
             self.types[category[0]][category[1]] = {
                 self.data_types[x][0]:
@@ -43,35 +53,57 @@ class AVAIL:
         self.const_to_dir = {}
         self.dir_to_const = {}
 
-    def set_size(self, var_type: str, size: int, is_global: bool):
+    def set_size(self, var_type: str, size: int, is_global: bool) -> None:
+        """
+        Funcion que asigna el tamano de la variable actual, para usar en el calculo de la proxima variable
+
+        :param var_type: el tipo de la variable que se quiere agrandar
+        :param size: el tamano a asignar a la variable
+        :param is_global: si la variable es global o local
+        :return: None
+        """
         if is_global:
             self.types["global"]["non_temp"][var_type]["size"] = size
         else:
             self.types["local"]["non_temp"][var_type]["size"] = size
 
-    def get_next(self):
-        self.counter = self.counter + 1
-        return f"tempvar-{self.counter - 1}"
-
     def get_next_global(self, var_type: str, is_temp: bool, var_name: str = ""):
+        """
+        Funcion que regresa la direccion virtual proxima para el tipo de dato en el contexto global
+
+        :param var_type: tipo de dato que se desea asignar
+        :param is_temp: si es un dato de tipo temporal
+        :param var_name: el nombre de la variable, se usa como llave para las variables temporales
+        :return: la direccion virtual proxima para el tipo de variable global
+        """
         if var_type in [x[0] for x in self.data_types]:
             if is_temp:
                 count = self.types.get("global").get("temp").get(var_type).get("counter")
                 minimum = self.types.get("global").get("temp").get(var_type).get("min")
                 self.types.get("global").get("temp").get(var_type)["counter"] += 1
+                # base + cantidad de variables asignada
                 return minimum + count
             else:
                 count = self.types.get("global").get("non_temp").get(var_type).get("counter")
                 minimum = self.types.get("global").get("non_temp").get(var_type).get("min")
                 size = self.types.get("global").get("non_temp").get(var_type).get("size")
+                # variables no temporales pueden ser dimensionadas, se debe tomar en cuenta el tamano de la anterior
                 self.types.get("global").get("non_temp").get(var_type)["counter"] += size
-
+                # la variable proxima esta en base + contador + tamano del anterior - 1 (se inicia en 0)
                 self.dir_to_var[str(minimum + count + size - 1)] = var_name
                 return minimum + count + size - 1
         else:
             print(f"ERROR. variable type {var_type} not recognized")
 
     def get_next_local(self, var_type: str, is_temp: bool, var_name: str = ""):
+        """
+        Funcion que regresa la direccion virtual proxima para el tipo de dato en el contexto global
+
+        :param var_type: tipo de dato que se desea asignar
+        :param is_temp: si es un dato de tipo temporal
+        :param var_name: el nombre de la variable, se usa como llave para las variables temporales
+        :return: la direccion virtual proxima para el tipo de variable local
+        """
         if var_type in [x[0] for x in self.data_types]:
             if is_temp:
                 count = self.types.get("local").get("temp").get(var_type)["counter"]
@@ -82,23 +114,35 @@ class AVAIL:
                 count = self.types.get("local").get("non_temp").get(var_type)["counter"]
                 minimum = self.types.get("local").get("non_temp").get(var_type)["min"]
                 size = self.types.get("global").get("non_temp").get(var_type).get("size")
+                # variables no temporales pueden ser dimensionadas, se debe tomar en cuenta el tamano de la anterior
                 self.types.get("local").get("non_temp").get(var_type)["counter"] += size
-
+                # la variable proxima esta en base + contador + tamano del anterior - 1 (se inicia en 0)
                 self.dir_to_var[str(count + minimum + size - 1)] = var_name
                 return count + minimum + size - 1
         else:
             print(f"ERROR. variable type {var_type} not recognized")
 
     def reset_locals(self):
+        """
+        Funcion que reinicia los contadores para las variables locales
+
+        :return: la cantidad de variables temporales que se eliminaron para cada tipo
+        """
+
+        # Esta funcion realiza el reseteo para las variables temporales o no temporales
         def reset(var_scope: str):
+            # Generar el diccionario de las variables y sus cantidades
             variables = {v_key: v_val.get("counter") for v_key, v_val in self.types.get("local").get(var_scope).items()
                          if v_val.get("counter") > 0}
+            # iterar sobre los tipos de variables, reiniciando el contador y eliminando del mapa anterior la variable
             for val_dir in self.types.get("local").get(var_scope):
                 counter = self.types.get("local").get(var_scope).get(val_dir)["counter"]
                 minim = self.types.get("local").get(var_scope).get(val_dir)["min"]
                 maxim = self.types.get("local").get(var_scope).get(val_dir)["max"]
+                # para las variables declaradas, eliminarla del diccionario de variables
                 for index in range(minim, minim + counter):
                     self.dir_to_var.pop(str(index), None)
+                # reiniciar el contador
                 self.types.get("local").get(var_scope).get(val_dir)["counter"] = 0
             return variables
 
@@ -106,7 +150,16 @@ class AVAIL:
         return reset("temp")
 
     def set_const_var(self, const_type: str, const_val: Any):
+        """
+        Funcion que genera y regresa la direccion de una constante. Si la constante ya existe, regresa la direccion ya
+        definida previamente.
+
+        :param const_type: el tipo de constante que crear
+        :param const_val: el valor a asignar a la constante
+        :return: regresa la direccion de la constante nueva o encontrada
+        """
         if const_type in [x[0] for x in self.data_types]:
+            # si no se encuentra la variable declarada, genearala
             if const_val not in self.const_to_dir:
                 const_dir = self.get_next_const(const_type, const_val)
                 self.const_to_dir[const_val] = const_dir
@@ -117,11 +170,19 @@ class AVAIL:
             print("ERROR. Const datatype not accepted")
 
     def get_next_const(self, var_type: str, value: Any):
+        """
+        Funcion que genera una direccion virtual para una constante
+
+        :param var_type: el tipo de variable a generar
+        :param value: el valor de la variable constante
+        :return: regresa la direccion de una constante
+        """
         if var_type in [x[0] for x in self.data_types]:
             if str(value) not in self.const_to_dir:
                 count = self.types.get("constant").get("value").get(var_type)["counter"]
                 minimum = self.types.get("constant").get("value").get(var_type)["min"]
                 self.types.get("constant").get("value").get(var_type)["counter"] += 1
+                # inicializa el diccionario de la constante para almacenar el tipo y valor
                 self.dir_to_const[str(count + minimum)] = {}
                 self.dir_to_const[str(count + minimum)]["value"] = value
                 self.dir_to_const[str(count + minimum)]["type"] = var_type
@@ -133,9 +194,11 @@ class AVAIL:
             print("ERROR. variable type not recognized")
 
     def get_const_type(self, mem_dir: int):
+        """Funcion que regresa el tipo de la variable en una direccion virtual"""
         return self.dir_to_const[str(mem_dir)]['type']
 
     def get_val_from_dir(self, var_dir: int):
+        """regresa el valor de una variable en una direccion, ya sea constante o variable"""
         if str(var_dir) in self.dir_to_const:
             return self.dir_to_const[str(var_dir)]["value"]
         elif str(var_dir) in self.dir_to_var:
